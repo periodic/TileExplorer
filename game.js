b2b0fb10b34016adafb0ea381fee75d4f88303ef
Drew Haven <drew.haven@gmail.com>
2014-08-17 19:58:59 -0700

    works without a webserver, but it's janky.

--- game.js
+++ game.js
@@ -2,8 +2,8 @@ require.config({
   urlArgs: "bust=" + (new Date()).getTime()
 });
 
-define(['lib/crafty', 'map', 'player', 'text!data/test.json'],
-       function(Crafty, Map, Player, tileJson) {
+define(['lib/crafty', 'map', 'player', 'data/test'],
+       function(Crafty, Map, Player, tiles) {
 console.log("Loading Game.");
 
 var size = 32 * 20;
@@ -19,7 +19,7 @@ function Game(width_px, height_px, tilesize) {
   };
 
   function start() {
-    var tiles = JSON.parse(tileJson);
+    //var tiles = JSON.parse(tileJson);
     this.map = Map(width_px / tilesize, height_px / tilesize, tilesize, tiles);
     this.player = Crafty.e('Player').at(3,3);
     var collisions = this.player.hit('Block');
