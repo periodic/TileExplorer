104fef3db680cdfb5f33f41b4e0cfa2efa1e84b7
Drew Haven <drew.haven@gmail.com>
2014-08-20 00:38:15 -0700

    Made it go to the next level when all objects are destroyed.

--- game.js
+++ game.js
@@ -6,22 +6,29 @@ define(['lib/crafty', 'map', 'player'],
        function(Crafty, Map, Player, tiles) {
 console.log("Loading Game.");
 
-var game = {};
-game.tile_size_pixels = 32;
-game.view_width_tiles = 20;
-game.view_height_tiles = 20;
-
-game.view_width_pixels = function () {
-  return this.tile_size_pixels * this.view_width_tiles;
-};
-game.view_height_pixels = function () {
-  return this.tile_size_pixels * this.view_height_tiles;
-};
-
-Crafty.init(game.view_width_pixels(), game.view_height_pixels());
-Crafty.background('black');
-Crafty.viewport.init(game.view_width_pixels(), game.view_height_pixels());
-Crafty.viewport.clampToEntities=false;
+Crafty.c('Score', {
+  _prefix: "Remaining: ",
+  init: function () {
+    this.requires('2D, Canvas, Text').attr({
+      x: 10,
+      y: 10,
+    }).textFont({weight: 'bold', size: '20px'})
+      .textColor('#FFFFFF')
+      .text(this._prefix)
+      .bind('UpdateScore', function () {
+        var remaining = Crafty('Destructable').length;
+        if (remaining == 0) {
+          Crafty.scene(this.next_scene);
+        } else {
+          this.text(this._prefix + Crafty('Destructable').length);
+        }
+      })
+      .bind('ViewportScroll', function (e) {
+        this.x = Math.max(0, 10 - Crafty.viewport.x);
+        this.y = Math.max(0, 10 - Crafty.viewport.y);
+      });
+  },
+});
 
 Crafty.scene("Level1", function () {
   require(['level1'], function (tiles) {
@@ -31,18 +38,12 @@ Crafty.scene("Level1", function () {
       game.tile_size_pixels,
       tiles);
 
-    var maxObj = Crafty('Destructable').length;
-    game.score = Crafty.e("2D, Canvas, Text").attr({
-      x: 10,
-      y: 10,
-    }).textFont({weight: 'bold', size: '20px'})
-      .textColor('#FFFFFF')
-      .text("Score: 0")
-      .bind('UpdateScore', function () {
-        this.text("Score: " + (maxObj - Crafty('Destructable').length));
-      });
+    game.score = Crafty.e("Score");
+    game.score.next_scene = 'Level2';
 
     Crafty.viewport.follow(Crafty('Player'), 0, 0);
+
+    Crafty.trigger('UpdateScore');
 
     console.log("Level 1 started.");
   });
@@ -56,22 +57,33 @@ Crafty.scene("Level2", function () {
       game.tile_size_pixels,
       tiles);
 
-    var maxObj = Crafty('Destructable').length;
-    game.score = Crafty.e("2D, Canvas, Text").attr({
-      x: 10,
-      y: 10,
-    }).textFont({weight: 'bold', size: '20px'})
-      .textColor('#FFFFFF')
-      .text("Score: 0")
-      .bind('UpdateScore', function () {
-        this.text("Score: " + (maxObj - Crafty('Destructable').length));
-      });
+    game.score = Crafty.e("Score");
+    game.score.next_scene = 'Level1';
 
     Crafty.viewport.follow(Crafty('Player'), 0, 0);
+
+    Crafty.trigger('UpdateScore');
 
     console.log("Level 2 started.");
   });
 });
+
+var game = {};
+game.tile_size_pixels = 32;
+game.view_width_tiles = 16;
+game.view_height_tiles = 16;
+
+game.view_width_pixels = function () {
+  return this.tile_size_pixels * this.view_width_tiles;
+};
+game.view_height_pixels = function () {
+  return this.tile_size_pixels * this.view_height_tiles;
+};
+
+Crafty.init(game.view_width_pixels(), game.view_height_pixels());
+Crafty.background('black');
+Crafty.viewport.init(game.view_width_pixels(), game.view_height_pixels());
+Crafty.viewport.clampToEntities = true;
 
 game.level1 = function () {
   Crafty.scene('Level1');
